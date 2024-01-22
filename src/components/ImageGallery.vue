f5c4886679c93605c4fc582eeabb39d489c127e3
TheFred94 <frederikrlarsen@gmail.com>
2024-01-22 16:10:10 +0100

    images are added more dynamically and are mapped out from an array of data

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -3,40 +3,43 @@
     class="overflow-auto whitespace-nowrap aspect-video flex flex-col flex-wrap h-[1000px] w-full"
     ref="imageGallery"
   >
-    <div class="h-full gallery-item">
+    <div
+      v-for="(image, index) in images"
+      :key="index"
+      class="h-full"
+    >
       <img
         class="h-full"
-        src="../../src/assets/images/image-1.jpg"
-        alt="Image 1"
-      />
-    </div>
-
-    <div class="h-full">
-      <img
-        class="h-full"
-        src="../../src/assets/images/image-2.jpg"
-        alt="Image 1"
-      />
-    </div>
-
-    <div class="h-full">
-      <img
-        class="h-full"
-        src="../../src/assets/images/image-3.jpg"
-        alt="Image 1"
-      />
-    </div>
-
-    <div class="h-full">
-      <img
-        class="h-full"
-        src="../../src/assets/images/image-4.jpg"
-        alt="Image 1"
+        :src="image.src"
+        :alt="image.alt"
       />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      images: [
+        { src: 'src/assets/images/image-1.jpg', alt: 'Image 1' },
+        { src: 'src/assets/images/image-2.jpg', alt: 'Image 2' },
+        { src: 'src/assets/images/image-3.jpg', alt: 'Image 3' },
+        { src: 'src/assets/images/image-4.jpg', alt: 'Image 4' },
+      ],
+    };
+  },
+  methods: {
+    handleScroll() {
+      const gallery = this.$refs.imageGallery;
+
+      if (gallery.scrollLeft + gallery.clientHeight >= gallery.scrollHeight) {
+        this.images = [...this.images, ...this.getMoreImages()];
+      }
+    },
+    getMoreImages() {
+      return this.images.map((image) => ({ ...image }));
+    },
+  },
+};
 </script>
